c67bfb3f6b87f7fad0209897c723d4990a5d5646
Daekuen <daekuenhan@gmail.com>
2023-01-03 11:38:03 +0900

    feat : page 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Videos from './pages/Videos';
+import VideoDetail from './pages/VideoDetail';
+import NotFound from './pages/NotFound';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <React.Fragment>
+      {/* <header>
+        <div></div>
+        <form action="submit">
+          <input type="text" />
+          <button>클릭</button>
+        </form>
+      </header> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Videos />} />
+          <Route path="/videos/*" element={<Videos />} />
+          <Route path="/watch/:videoId" element={<VideoDetail />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
+  );
 }
 
 export default App;
